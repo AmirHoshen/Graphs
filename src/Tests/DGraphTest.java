aa89ad731cd0a1a72134ac14a9d27b8e14328df0
amir hoshen <mi.rosh160@gmail.com>
2019-12-18 18:02:14 +0200

    empty project 1st initialized

---
+++ src/Tests/DGraphTest.java
@@ -0,0 +1,50 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DGraphTest {
+
+    @org.junit.jupiter.api.Test
+    void getNode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEdge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addNode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void connect() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getV() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getE() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeNode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeEdge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nodeSize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void edgeSize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMC() {
+    }
+}
