b27ee2a67161ee5e7420424f192f4c52a4c8cc27
amir hoshen <mi.rosh160@gmail.com>
2019-12-18 19:25:57 +0200

    node data missing 4 methods

--- src/elements/nodeData.java
+++ src/elements/nodeData.java
@@ -3,30 +3,61 @@ package elements;
 import dataStructure.node_data;
 import utils.Point3D;
 
+import java.io.InvalidObjectException;
+
 public class nodeData implements node_data {
+    private static int _id = 0;
+    private double _weight;
+    private Point3D _location;
+    private String _info = "";
+    private int _tag;
+
+
+    public nodeData(){
+        this._id++;
+        _weight = 0;
+        _location = new Point3D(0,0);
+    }
+
+    public nodeData(double weight,Point3D location){
+        this._id++;
+        _weight = weight;
+        _location = location;
+    }
+
+    public nodeData(nodeData nd){
+        this._id++;
+        this._weight = nd.getWeight();
+        this._location = new Point3D(nd.getLocation());
+        this._tag = nd.getTag();
+        this._info = nd.getInfo();
+    }
+
     @Override
     public int getKey() {
-        return 0;
-    }
-
-    @Override
-    public Point3D getLocation() {
-        return null;
-    }
-
-    @Override
-    public void setLocation(Point3D p) {
-
+        return _id;
     }
 
     @Override
     public double getWeight() {
-        return 0;
+        return this._weight;
     }
 
     @Override
     public void setWeight(double w) {
+        this._weight = w;
+    }
 
+    @Override
+    public Point3D getLocation() {
+        return this._location;
+    }
+
+    @Override
+    public void setLocation(Point3D p) {
+        if(p!=null){
+            this._location = new Point3D(p);
+        }
     }
 
     @Override
