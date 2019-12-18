9c28275e92301e74cba18fd27bff06846723df3d
amir hoshen <mi.rosh160@gmail.com>
2019-12-18 18:08:14 +0200

    methods ready to be initialized

---
+++ src/elements/nodeData.java
@@ -0,0 +1,51 @@
+package elements;
+
+import dataStructure.node_data;
+import utils.Point3D;
+
+public class nodeData implements node_data {
+    @Override
+    public int getKey() {
+        return 0;
+    }
+
+    @Override
+    public Point3D getLocation() {
+        return null;
+    }
+
+    @Override
+    public void setLocation(Point3D p) {
+
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWeight(double w) {
+
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public void setInfo(String s) {
+
+    }
+
+    @Override
+    public int getTag() {
+        return 0;
+    }
+
+    @Override
+    public void setTag(int t) {
+
+    }
+}
