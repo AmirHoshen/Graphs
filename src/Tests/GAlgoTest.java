c2aea78179656025608abc99632dcef5012e2dea
Peleg Z <peleg122@gmail.com>
2020-01-03 19:54:35 +0200

    Graph_Algo Tests added into GAlgoTest

--- src/Tests/GAlgoTest.java
+++ src/Tests/GAlgoTest.java
@@ -6,65 +6,129 @@ import dataStructure.graph;
 import dataStructure.node_data;
 import elements.nodeData;
 import gui.Graph_GUI;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import utils.Point3D;
 
+import java.lang.reflect.AccessibleObject;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class GAlgoTest {
-    @Test
-    public void GAlgoTest() {
-        graph graph = new DGraph();
-        Graph_Algo g = new Graph_Algo();
+    public static graph g;
 
-        node_data n1 = new nodeData(new Point3D(50, 10));
-        node_data n2 = new nodeData(new Point3D(10, 10));
-        node_data n3 = new nodeData(new Point3D(50, 40));
-        node_data n4 = new nodeData(new Point3D(33, 25));
-        node_data n5 = new nodeData(new Point3D(10, 40));
-        node_data n6 = new nodeData(new Point3D(33, 50));
+    @BeforeAll
+    public static void setUp() {
+        g = new DGraph();
 
-        graph.addNode(n1);
-        graph.addNode(n2);
-        graph.addNode(n3);
-        graph.addNode(n4);
-        graph.addNode(n5);
-        graph.addNode(n6);
+        node_data n1 = new nodeData(new Point3D(10, 10));
+        node_data n2 = new nodeData(new Point3D(15, 10));
+        node_data n3 = new nodeData(new Point3D(10, 20));
 
-        graph.connect(0, 3, 1);
-        graph.connect(3, 4, 5);
-        graph.connect(4, 5, 1);
-        graph.connect(5, 2, 6);
-        graph.connect(2, 3, 3);
-        graph.connect(3, 1, 4);
-        graph.connect(1, 0, 2);
-        graph.connect(3, 0, 10);
-        g.init(graph);
-        System.out.println(g.isConnected());
+        g.addNode(n1);
+        g.addNode(n2);
+        g.addNode(n3);
 
-        graph graph1 = new DGraph();
-        Graph_Algo g1 = new Graph_Algo();
-
-        node_data n11 = new nodeData(new Point3D(50, 10));
-        node_data n21 = new nodeData(new Point3D(10, 10));
-
-        graph1.addNode(n11);
-        graph1.addNode(n21);
-
-        graph1.connect(0, 1, 1);
-        graph1.connect(1, 0, 5);
-
-        g1.init(graph1);
-        System.out.println(g1.isConnected());
-        g.shortestPathToString(g.shortestPath(2, 0));
-        g.save("test.txt");
-        Graph_Algo t = new Graph_Algo();
-        t.init("test.txt");
-        t.shortestPathToString(t.shortestPath(0, 1));
+        g.connect(0, 1, 1);
+        g.connect(1, 2, 2);
+        g.connect(2, 0, 3);
     }
+
+    @Test
+    public void isConnected() {
+        Graph_Algo ga = new Graph_Algo();
+        ga.init(g);
+
+        if (!ga.isConnected()) {
+            fail("Should be Connected!");
+        } else {
+            System.out.println("isConnected is all Good!");
+        }
+    }
+
+    @Test
+    public void ShortestPath() {
+        Graph_Algo ga = new Graph_Algo(g);
+        String Expected = "Path: 0 >> 1 >> 2 | Distance: 3.0";
+        String Actual = ga.shortestPathToString(ga.shortestPath(0, 2));
+        if (!Expected.equals(Actual)) {
+            fail("Shortest Path doesnt Work");
+        } else {
+            System.out.println("Shortest Path working");
+        }
+    }
+
+    @Test
+    public void ShortestPathDist() {
+        Graph_Algo ga = new Graph_Algo(g);
+        String Expected = "5.0";
+        String Actual = "" + ga.shortestPathDist(1, 0);
+        if (!Expected.equals(Actual)) {
+            fail("Shortest Path Dist doesnt Work");
+        } else {
+            System.out.println("Distance: " + Actual);
+            System.out.println("Shortest Path Dist working");
+        }
+    }
+
+    @Test
+    public void SaveAndLoad() {
+        Graph_Algo ga = new Graph_Algo(g);
+        ga.save("tester.txt");
+        Graph_Algo gaSave = new Graph_Algo();
+        gaSave.init("tester.txt");
+        String Expected = ga.shortestPathToString(ga.shortestPath(0, 1));
+        String Actual = gaSave.shortestPathToString(gaSave.shortestPath(0, 1));
+        if (!Expected.equals(Actual)) {
+            fail("Save And Load doesnt Work!");
+        } else {
+            System.out.println("Save and Load(init) working");
+        }
+    }
+    @Test
+    public void TSP(){
+        Graph_Algo ga = new Graph_Algo(g);
+        String Expected = "Path: 0 >> 2 >> 1 >> 1 >> 0 >> 2 | Distance: 4.0";
+        ArrayList<Integer> arrayList = new ArrayList();
+        arrayList.add(0);
+        arrayList.add(1);
+        arrayList.add(2);
+        String Actual = ga.shortestPathToString(ga.TSP(arrayList));
+        if (!Expected.equals(Actual)) {
+            fail("TSP doesnt Work");
+        } else {
+            System.out.println("TSP working");
+        }
+    }
+
+    @Test
+    public void copy() {
+        Graph_Algo ga = new Graph_Algo();
+        nodeData n = new nodeData(new Point3D(20,30));
+        ga._graph.addNode(n);
+        Graph_Algo gCopy = new Graph_Algo();
+        ga.init(g);
+        gCopy.init(ga.copy());
+        int Expected = ga._graph.nodeSize();
+        int Actual = gCopy._graph.nodeSize();
+        if (Expected  != Actual) {
+            fail("copy Doesnt Working!");
+        } else {
+            System.out.println("copy Working!");
+        }
+
+    }
+
 
     public static void main(String[] args) {
         Graph_GUI gg = new Graph_GUI();
         graph graph = new DGraph();
-        Graph_Algo g = new Graph_Algo();
+        Graph_Algo g1 = new Graph_Algo();
 
         node_data n1 = new nodeData(new Point3D(-20, -20));
         node_data n2 = new nodeData(new Point3D(40, 40));
@@ -100,9 +164,8 @@ public class GAlgoTest {
         graph.connect(1, 2, 25);
         graph.connect(6, 0, 5);
 
-        g.init(graph);
+        g1.init(graph);
         gg.init(graph);
-        g.shortestPathToString(g.shortestPath(1, 4));
         gg.drawGraph();
     }
 }
